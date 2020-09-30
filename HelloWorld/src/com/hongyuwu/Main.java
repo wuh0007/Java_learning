7bbf0fa644e61123becaf80abe83024ac39782e1
wuh0007 <wuh007@udayton.edu>
2020-09-30 12:56:04 -0400

    1st commit

---
+++ HelloWorld/src/com/hongyuwu/Main.java
@@ -0,0 +1,11 @@
+package com.hongyuwu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+	    System.out.println("Hello World");
+    }
+}
